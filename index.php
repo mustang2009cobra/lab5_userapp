b1ca288803f4922464fe5a70d784854ce6759912
David Woodruff <mustang2009cobra@gmail.com>
2013-04-02 10:51:07 -0600

    Added temporary index.php file

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: David
+ * Date: 4/2/13
+ * Time: 10:50 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo "Temporary Index File";
