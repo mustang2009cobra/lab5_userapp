e39ce0917dbb98fd4471bfc2bcffa4431fe63edf
Ogden Mills <ogdenlaynemills@gmail.com>
2013-04-13 17:57:41 -0600

    added the phone field.

--- sql/user-app-create-db.sql
+++ sql/user-app-create-db.sql
@@ -12,6 +12,7 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
+  `phone` varchar(14) NOT NULL,
   `password` varchar(128) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1$$
