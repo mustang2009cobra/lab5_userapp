cadc67c1d87458adbcca51ab9bfa16fc1d2b5a5a
Ogden Mills <ogdenlaynemills@gmail.com>
2013-04-14 16:41:19 -0600

    adding in event receiver

---
+++ application/controllers/eventController.php
@@ -0,0 +1,18 @@
+<?php
+
+class eventsController extends CI_Controller {
+	
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('users_model');
+	}
+	
+	public function recieve(){
+        $data = array();
+
+		$this->load->view('templates/header');
+		$this->load->view('pages/events', $data);
+		$this->load->view('templates/footer');
+	}
+
+}
---
+++ application/views/pages/events.php
@@ -0,0 +1,45 @@
+<?php
+
+?>
+
+<div class="navbar navbar-inverse navbar-fixed-top">
+  <div class="navbar-inner">
+	<div class="container">
+	  <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	  </a>
+	  <a class="brand" href="<?= site_url('/main/index'); ?>">User App</a>
+	  <div class="nav-collapse collapse">
+		<ul class="nav pull-right">
+		  <li><a href="<?= site_url('/main/login'); ?>">Login/Register</a></li>
+		</ul>
+	  </div><!--/.nav-collapse -->
+	</div>
+  </div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="span2"></div>
+		<div class="span8">
+			<h1>Events</h1>
+		</div>
+		<div class="span2"></div>
+	</div>
+	<div class="row">
+		<div class="span2"></div>
+		<div class="span8">
+			<h3>Events</h3>
+			<p>Notifications:</p>
+		</div>
+		<div class="span2"></div>
+	</div>
+</div> <!-- /container -->
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		//Any page JS goes here
+	});
+</script>
