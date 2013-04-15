9d02cdf7ad8419b95ab18e605e317745089e6b08
Ogden Mills <ogdenlaynemills@gmail.com>
2013-04-14 23:23:31 -0600

    adding helpers

--- application/controllers/eventController.php
+++ application/controllers/eventController.php
@@ -5,6 +5,7 @@ class eventController extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
 		$this->load->model('users_model');
+		$this->load->helper('textHelper');
 	}
 	
 	public function recieve(){
@@ -25,38 +26,7 @@ class eventController extends CI_Controller {
 		parseEvent($formData);
 	}
 	
-	function parseEvent($formData){
-		if(!isset($formData["email"])||!isset($formData["message"]))
-			return;
-			
-		$email = $formData["email"];
-		$message = $formData["message"];
-		
-		//lookup number of first user found. 
-		$number = $this->users_model->getNumber($email);
-		
-		sendText($number, $message);
-		
-	}
 	
-	function sendText($to, $message){
-	
-		require("../twilio-php/Services/Twilio.php");
-		
-		$from = "+14357280603";
-		$to = "+".$to;
-		$sid = "AC8dcc306c08bac183a3f450f52ae759f2";
-		$token = "ab8826ec37ae8ebc16542d51a21014ed";
-		
-		recordError("prepping sms to: ".$to." message:".$message);
-		
-		$client = new Services_Twilio($sid, $token);
-		$message = $client->account->sms_messages->create($from, $to, $message, array());
-		
-		//recordError("Sent message - ".$message->sid);
-	
-	
-	}
 
 
 }
