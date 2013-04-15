13f981f44ad7d589759108f47242a69529f76ba3
Ogden Mills <ogdenlaynemills@gmail.com>
2013-04-15 00:08:12 -0600

    fixed name

--- application/helpers/event_helper.php
+++ application/helpers/event_helper.php
@@ -10,7 +10,7 @@
 		//lookup number of first user found. 
 		$user = $model->users_model->getNumber($email);
 		print_r($user);
-		sendText($user->number, $message, $model);
+		sendText($user->phone, $message, $model);
 		
 	}
 	
